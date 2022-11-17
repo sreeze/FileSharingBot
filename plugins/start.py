8af0401460e6b343d645fff183d9d4ddc0c64ccd
sreeze <117290673+sreeze@users.noreply.github.com>
2022-11-17 19:38:25 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,7 +9,7 @@ from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, OWNER_ID, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
-from database.sql import add_user, query_msg, full_userbase
+from database.database import add_user, query_msg, full_userbase
 
 
 #=====================================================================================##
