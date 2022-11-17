fa1333632cfbd994a0736fb42b6436d12ca28338
sreeze <117290673+sreeze@users.noreply.github.com>
2022-11-17 18:17:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,10 +61,10 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "❌Don't send me messages directly Join @CinemaCompanyofficials !"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(1068173302)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
