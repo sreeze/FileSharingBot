c252796384bcae76d0e5935e2321f50432416f82
sreeze <117290673+sreeze@users.noreply.github.com>
2024-05-14 18:32:00 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>SH</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/CinemaCompanyofficials'>Click here</a>\n○ Channel : @CinemaCompanyOffiz\n○ Support Group : @CinemaCompanyMovie</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>SH</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/CinemaCompanyofficials'>Click here</a>\n○ Channel : @CinemaCompanyUpdates\n○ Support Group : @CinemaCompanyMovie</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
